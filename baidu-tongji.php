531ef6e9ffc58cba3fb7bd74771e6c956c41de6b
wayn3h0 <me@wayn3h0.com>
2016-05-17 13:49:05 +0800

    add position config

--- baidu-tongji.php
+++ baidu-tongji.php
@@ -23,15 +23,21 @@ class BaiduTongjiPlugin extends Plugin
         $trackingID = trim($this->config->get('plugins.baidu-tongji.trackingID'));
         if ($trackingID) {
             $init = "
-              var _hmt = _hmt || [];
-              (function() {
-              var hm = document.createElement('script');
-              hm.src = '//hm.baidu.com/hm.js?{$trackingID}';
-              var s = document.getElementsByTagName('script')[0];
-              s.parentNode.insertBefore(hm, s);
-              })();
-            ";
-            $this->grav['assets']->addInlineJs($init);
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement('script');
+hm.src = '//hm.baidu.com/hm.js?{$trackingID}';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(hm, s);
+})();
+";
+
+            $position = trim($this->config->get('plugins.baidu-tongji.position'));
+            if ($position == '' || $position == 'head') {
+              $this->grav['assets']->addInlineJs($init);
+            } else {
+              $this->grav['assets']->addInlineJs($init, null, $position);
+            }
         }
     }
 }
